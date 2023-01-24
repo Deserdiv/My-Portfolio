eb65ac732bed60b6757a4907402dd79eac2eaee9
Deserdiv <116967915+Deserdiv@users.noreply.github.com>
2023-01-24 22:28:14 +0300

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -9,28 +9,28 @@ export const data=[
         name:"Weather App",
         image:weather,
         github:"https://github.com/Deserdiv/React-Weather-App",
-        live:"https://benevolent-fudge-1b1741.netlify.app/",
+        live:"https://weatherappdeserdiv.netlify.app/",
     },
     {
         id:2,
         name:"TO-DO App",
         image:todo,
         github:"https://github.com/Deserdiv/React-To-Do-App",
-        live:"https://dainty-souffle-849672.netlify.app/",
+        live:"https://tododeserdiv.netlify.app/",
     },
     {
         id:3,
         name:"Youtube Clone",
         image:youtubeclone,
         github:"https://github.com/Deserdiv/React_Youtube-Clone",
-        live:"https://dulcet-boba-932229.netlify.app/",
+        live:"https://deserdiv-landing.netlify.app/",
     },
     {
         id:4,
         name:"Landing Page",
         image:landingpage,
         github:"https://github.com/Deserdiv/React-Landing-Page",
-        live:"https://sparkling-kleicha-9e9d02.netlify.app/",
+        live:"https://deserdiv-landing.netlify.app/",
     },
 
 
